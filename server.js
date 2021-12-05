44d26f36a927c30952931a1446b2fbfaaa062419
Montii <llh9@yahoo.com>
2021-12-05 13:27:07 -0500

    Added Walkthrough video

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
 const sequelize = require('./config/connection');
-//const seedall = require('./seeds/index');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -11,11 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-//const seedAll = require('./seeds/index')
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false })
 .then(() => {
-  // seedall.seedAll();
   app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
